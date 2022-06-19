f9c4fe6fa2a81d334ccb761428d7c853b879305b
Silvani Fernandes Gonçalves <silvanibjj@gmail.com>
2022-06-19 19:10:10 -0300

    Correção de bug.

--- services/countDays.js
+++ services/countDays.js
@@ -6,14 +6,14 @@ const countDays = (infoInitialDate, infoFinalDate, day) => {
   let currentDate = formatDate(infoInitialDate);
   const timestampFinal = formatDate(infoFinalDate);
   let dayCounter = 0;
-  while(currentDate < timestampFinal) {
+  while(currentDate <= timestampFinal) {
     const day = new Date(currentDate);
     if (!daysNumbers || daysNumbers.includes(day.getDay())) {
       dayCounter += 1;
     }
     currentDate += 86400000;
   }
-  return day ? dayCounter : dayCounter +1
+  return dayCounter
 }
 
 module.exports = { countDays };
